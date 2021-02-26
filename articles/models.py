acde7e4ffb04712273dc7a077a277f32497fe701
Ioannis Apostolou <giannistolou@protonmail.com>
2021-02-26 20:27:06 +0200

    add text editor to content

--- articles/models.py
+++ articles/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from datetime import datetime
+from tinymce.models import HTMLField
 
 # Create your models here.
 class Article(models.Model):
@@ -8,7 +9,7 @@ class Article(models.Model):
     slug = models.SlugField(max_length=200, unique=True, help_text="The url of the article. Must be unique!", default=datetime.now().strftime("%d-%m-%y") + '/')
     author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_article')
     created_date = models.DateTimeField('date published')
-    content = models.TextField()
+    content = HTMLField()
     updated_on = models.DateTimeField(auto_now= True)
     image = models.ImageField(upload_to='uploads/% d/% m/% y/')
     
