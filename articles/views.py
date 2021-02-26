b2c88ff1a85b18b6d2bb095b020930319d1cd3ee
Ioannis Apostolou <giannistolou@protonmail.com>
2021-02-27 00:26:54 +0200

    add index template

--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,6 @@
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the articles index.")
+    return render(request, 'index.html')
