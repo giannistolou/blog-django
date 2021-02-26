1d861eda18627c7b68207f4fee87fbc6659522f1
Ioannis Apostolou <giannistolou@protonmail.com>
2021-02-26 19:43:51 +0200

    create the basic of an article

---
+++ README.md
@@ -0,0 +1,23 @@
+# Description
+
+Bla bla bla
+
+## Installation
+
+Use the package manager [x](x) to install y.
+
+```bash
+pip install x
+```
+
+## Usage
+
+```python
+print 'python code'
+```
+
+## Libraries
+We use this...
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model, ):
+    title = models.TextField(max_length=200)
+    publication_date = models.DateTimeField('date published')
+    text = models.TextField()
+    image = models.ImageField(upload_to='uploads/% Y/% m/% d/')
