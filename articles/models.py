32449235b9ad1e0c42e06a0b174e597e32737f7f
Ioannis Apostolou <giannistolou@protonmail.com>
2021-02-27 11:36:01 +0200

    create the template of the blog and change editor from tinymce to ckeditor

--- articles/models.py
+++ articles/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from datetime import datetime
-from tinymce.models import HTMLField
+from ckeditor.fields import RichTextField
+from datetime import datetime
 
 # Create your models here.
 STATUS = (
@@ -26,14 +27,13 @@ class Tag(models.Model):
 class Article(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     title = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=200, unique=True, help_text="The url of the article. Must be unique!", default=datetime.now().strftime("%d-%m-%y") + '/')
     author = models.ForeignKey(User, on_delete= models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     tags = models.ManyToManyField(Tag)
     created_date = models.DateTimeField('date published')
-    content = HTMLField()
+    content = RichTextField(blank=True, null = True)
     updated_on = models.DateTimeField(auto_now= True)
-    image = models.ImageField(upload_to='uploads/% d/% m/% y/')
+    image = models.ImageField(upload_to='articles/uploads/% d/% m/% y/')
 
     def __str__(self):
         return self.title
--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,12 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from .models import Article
 
 
 def index(request):
     return render(request, 'index.html')
+
+def blog(request):
+    articles = Article.objects.order_by('created_date')[:5]
+    context = {'articles': articles}
+    return render(request, 'blog.html', context)
