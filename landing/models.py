b79322ad19ced17529a3d2f016b70c5a492eb602
Ioannis Apostolou <giannistolou@protonmail.com>
2021-03-03 17:21:11 +0200

    return the name of the project and of the skill for the admin panel

--- landing/models.py
+++ landing/models.py
@@ -10,9 +10,15 @@ class Skill(models.Model):
     name= models.CharField(max_length = 100)
     knowledge = models.IntegerField(choices=KNOWLEDGE, default=0)
 
+    def __str__(self):
+        return self.name
+
 class Project(models.Model):
     name = models.CharField(max_length = 100)
     context = models.TextField(max_length = 300)
     link = models.URLField(max_length = 300, blank = True)
+
+    def __str__(self):
+        return self.name
 
 
